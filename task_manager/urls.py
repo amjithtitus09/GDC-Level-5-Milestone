38e9a7fc12cc1d76e6e2e5a8526e8c8227cca5f1
Amjith Titus <amjithtitus09@gmail.com>
2023-04-26 20:12:18 +0530

    Submission

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import tasks_view, add_task_view, all_tasks_view, complete_task_view, completed_tasks_view, delete_task_view
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('tasks/', tasks_view),
+    path('add-task/', add_task_view),
+    path('delete-task/<int:index>/', delete_task_view),
+    path('complete_task/<int:index>/', complete_task_view),
+    path('completed_tasks/', completed_tasks_view),
+    path('all_tasks/', all_tasks_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,35 @@
-# Add your Views Here
+# Add all your views here
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+from tasks.models import Task
+
+tasks = []
+
+def tasks_view(request):
+    tasks = Task.objects.filter(completed=False).filter(deleted=False)
+    search_term = request.GET.get("search")
+    if search_term:
+        tasks = tasks.filter(title__icontains=request.GET.get("search"))
+    return render(request, "tasks.html", {"tasks" : tasks})
+
+def completed_tasks_view(request):
+    completed_tasks = Task.objects.filter(completed=True).filter(deleted=False)
+    return render(request, "completed_tasks.html", {"completed_tasks" : completed_tasks})
+
+def all_tasks_view(request):
+    tasks = Task.objects.filter(completed=False).filter(deleted=False)
+    completed_tasks = Task.objects.filter(completed=True).filter(deleted=False)
+    return render(request, "all_tasks.html", {"completed_tasks" : completed_tasks, "tasks" : tasks})
+
+def add_task_view(request):
+    Task(title = request.GET.get("task")).save()
+    return HttpResponseRedirect("/tasks")
+
+def delete_task_view(request, index):
+    Task.objects.filter(id=index).update(deleted=True)
+    return HttpResponseRedirect("/tasks")
+
+def complete_task_view(request, index):
+    Task.objects.filter(id=index).update(completed=True)
+    return HttpResponseRedirect("/tasks")
